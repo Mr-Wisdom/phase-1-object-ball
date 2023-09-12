7c20b89718a63a115cab72aed47b45ed81db7451
AndreMarrero <andremjr65@gmail.com>
2023-09-12 09:31:44 -0400

    finished home team half of Part 1

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -5,15 +5,66 @@ function gameObject() {
     home: {
         teamName: Brooklyn-Nets ,
         teamColor:[Black, White],
-        players:{
+        players: {
+            "Alan Anderson" : {
+                Number : 0,
+                Shoe : 16,
+                Points : 22,
+                Rebounds : 12,
+                Assists : 12,
+                Steals : 3,
+                Blocks : 1,
+                SlamDunks : 1,
+            },
+            "Reggie Evans" : {
+                Number : 30,
+                Shoe : 14,
+                Points : 12,
+                Rebounds : 12,
+                Assists : 12,
+                Steals : 12,
+                Blocks : 12,
+                SlamDunks : 7,
+            },
+            "Brook Lopez" : {
+                Number : 11,
+                Shoe : 17,
+                Points : 17,
+                Rebounds : 19,
+                Assists : 10,
+                Steals : 3,
+                Blocks : 1,
+                SlamDunks : 15,
+            },
+            "Mason Plumlee" : {
+                Number : 1,
+                Shoe : 19,
+                Points : 26,
+                Rebounds : 12,
+                Assists : 6,
+                Steals : 3,
+                Blocks : 8,
+                SlamDunks : 5,
+            },
+            "Jason Terry" : {
+                Number : 31,
+                Shoe : 15,
+                Points : 19,
+                Rebounds : 2,
+                Assists : 2,
+                Steals : 4,
+                Blocks : 11,
+                SlamDunks : 1,
+            }
+        }
+ 
             
-
         }
 
 
 
     },
-    away:{
+    away: {
         teamName: Charlotte-Hornets,
         teamColor: [Turquoise, Purple],
         players: {} 
