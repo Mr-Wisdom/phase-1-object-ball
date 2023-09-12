9bfc69f2c638b251711da587bc90075cbe9c8f13
Rarii Mathis <juniormathis82@gmail.com>
2023-09-12 15:52:55 -0400

    src

---
+++ src/00-objectball.js
@@ -0,0 +1,22 @@
+const { blobToBuffer } = require("jsdom");
+
+function gameObject() {
+  const teams = {
+    home: {
+        teamName: Brooklyn-Nets ,
+        teamColor:[Black, White],
+        players:{
+            
+
+        }
+
+
+
+    },
+    away:{
+        teamName: Charlotte-Hornets,
+        teamColor: [Turquoise, Purple],
+        players: {} 
+    }
+  }  
+}
